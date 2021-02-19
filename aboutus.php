43a836a2586c6c91fff46225a6f5372ae3f31729
cdtsbikaner <cdtsbikaner@gmail.com>
2021-02-19 19:32:53 +0530

    initial commit for my Program

--- aboutus.php
+++ aboutus.php
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-Hi Demo data
-<<<<<<< HEAD
-hi from aakash
 added new lines to aboutus.php file
 checking the file. 
-=======
-Hi this another test
->>>>>>> aks
-=======
-Now its final time i am modify this 
->>>>>>> aks
+THIS LINE WROTE FOR HARMANI
